3d06e8b866f51a33e16c522bcdd452420f1c954b
sandy1983jsr <sandeep.uce208@gmail.com>
2025-08-05 21:59:15 +0530

    Update boilers.py

--- modules/boilers.py
+++ modules/boilers.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 from utils.data_loader import load_csv
 
 def boilers_tab():
@@ -9,13 +8,11 @@ def boilers_tab():
         df = load_csv(uploaded_file)
     else:
         df = load_csv("data/sample/boilers_sample.csv")
-    
     st.subheader("Raw Data")
     st.dataframe(df)
     st.metric("Total Steam Generated (kg)", df["Steam_kg"].sum())
     st.metric("Boiler Efficiency (%)", round(df["Efficiency"].mean(),2))
     st.area_chart(df[["Timestamp", "Fuel_Consumed"]].set_index("Timestamp"))
-    
     st.subheader("Efficiency Calculator")
     fuel = st.number_input("Fuel Used (kg)", value=100)
     steam = st.number_input("Steam Generated (kg)", value=150)

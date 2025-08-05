5e70edf0cd3b2b3e958dadc3d03f959c6c4a5f79
sandy1983jsr <sandeep.uce208@gmail.com>
2025-08-05 21:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,27 @@ from modules.boilers import boilers_tab
 from modules.cip import cip_tab
 from modules.air_compressors import air_compressors_tab
 from modules.lighting_hvac import lighting_hvac_tab
+from modules.advanced_analytics import advanced_analytics_tab
 from utils.alerts import show_alerts
 from utils.ai_recommendations import show_ai_recommendations
-from style import set_custom_style
+from utils.style import set_custom_style
 
-st.set_page_config(page_title="Dairy Energy Digital Twin", layout="wide")
+st.set_page_config(page_title="Dairy Digital Twin", layout="wide")
 set_custom_style()
 
 st.title("🧑‍🔬 Dairy Plant Energy Digital Twin")
 st.sidebar.image("https://heritagefoods.in/img/logo.png", width=180)
 st.sidebar.markdown("## Navigation")
-tab_names = ["Refrigeration & Chilling", "Pasteurization/Boilers", "Water Heating & CIP", "Air Compressors", "Lighting & HVAC", "Alerts", "AI Recommendations"]
+tab_names = [
+    "Refrigeration & Chilling",
+    "Pasteurization/Boilers",
+    "Water Heating & CIP",
+    "Air Compressors",
+    "Lighting & HVAC",
+    "Advanced Analytics",
+    "Alerts",
+    "AI Recommendations"
+]
 tabs = st.tabs(tab_names)
 
 with tabs[0]:
@@ -28,6 +38,8 @@ with tabs[3]:
 with tabs[4]:
     lighting_hvac_tab()
 with tabs[5]:
-    show_alerts()
+    advanced_analytics_tab()
 with tabs[6]:
+    show_alerts()
+with tabs[7]:
     show_ai_recommendations()
